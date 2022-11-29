b2dfd6f916be9d799e39447836ce0fd402c5d7d9
favour2333 <kingsleyfavour999@gmail.com>
2022-11-29 14:51:44 +0000

    task 7

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_first_3 = word[:3]
+word_last_2 = wordl-2:
+middle_word = word[1:-1]
+print("first 3 letters: {}".format(word_first_3))
+print("last 2 letters: {}".format(word_last_2))
+print(middle word: {}".format(middle_word))
+ 2022 github, Inc.
+Footer navigation
+Terms
+privacy
+secu 
