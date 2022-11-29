e49c049335d89cfd8c5c76602554185b0413ca6a
favour2333 <kingsleyfavour999@gmail.com>
2022-11-29 20:31:08 +0000

    task 4

---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# Author - kingsley favour 
+
+"""print numbers 0 to 98 to decimal and hexadecimal."""
+for number in range(0, 99):
+    print("{} . {}".format(number, hex(number)))
+
