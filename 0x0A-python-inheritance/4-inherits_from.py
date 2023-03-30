274f85c107aa391cf1e460cabf2b0757324f8daa
favour2333 <kingsleyfavour999@gmail.com>
2023-03-30 12:20:27 +0000

    initial

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,19 +1,13 @@
 #!/usr/bin/python3
+"""
+This module implements  a function that returns True if the object
+is an instance of a class that inherited (directly or indirectly)
+from the specified class ; otherwise False.
+"""
 
-inherits_from = __import__('4-inherits_from').inherits_from
 
+def inherits_from(obj, a_class) -> bool:
+    """implementation
+    """
+    return type(obj) != a_class and issubclass(type(obj), a_class)
 
-
-a = True
-
-if inherits_from(a, int):
-
-        print("{} inherited from class {}".format(a, int.__name__))
-
-        if inherits_from(a, bool):
-
-                print("{} inherited from class {}".format(a, bool.__name__))
-
-                if inherits_from(a, object):
-
-                        print("{} inherited from class {}".format(a, object.__name__))
--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,19 +1,11 @@
 #!/usr/bin/python3
-
 """
-
 This module implements an object skeleton for BaseGeometry
-
 """
-
-
-
 
 
 class BaseGeometry:
+    """skeleton
+    """
+    pass
 
-        """skeleton
-
-            """
-
-                pass
